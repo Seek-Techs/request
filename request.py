60dd37a9494ffae6e2bb50c406ea617174530d3b
Sikiru Yusuff <sikiru.yusuff@yahoo.com>
2022-11-25 16:58:10 +0100

    update

--- request.py
+++ request.py
@@ -4,37 +4,62 @@ import ssl
 import smtplib
 import json
 from account import *
+import os
 
-
-email_sender = emailsender
-email_password = emailpassword
+API_ENDPOINT = 'https://user-mangement-api.herokuapp.com/'
+SMTP_HOST = 'smtp.gmail.com'
+EMAIL_SENDER = emailsender
+EMAIL_PASSWORD = emailpassword
+email_receiver = []
+print('running')
 
 with open('ss.json', 'r') as file:
     data = json.load(file)
     for key, value in data.items():
+        
         recipient = value
-        #print(recipient)
 
-        email_reciever = recipient
-        print(email_reciever)
+        email_receiver.append(recipient) 
+        print(value)
 
-payload = {'username': 'sikiru', 'password': 'honest'}
-response = requests.post('https://httpbin.org/post', data=payload)
+
+
 # with open('mytext.txt', 'a') as file:
 #     file.write(response.text)
 # dicts = response.json()
 # print(dicts['form'])
 
-subject = 'request'
-body = 'successfully posted'
-em = EmailMessage()
-em['from'] = email_sender
-em['to'] = ', '.join(email_reciever)
-em['subject'] = subject
-em.set_content(body)
+def send_update(msg_body, json):
+    subject = 'post request'
+    body = 'successfully posted'
+    em = EmailMessage()
+    em['from'] = EMAIL_SENDER
+    em['to'] = ', '.join(email_receiver)
+    em['subject'] = subject
+    em.set_content(body)
 
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-    smtp.login(email_sender, email_password)
-    smtp.sendmail(email_sender, email_reciever, em.as_string())
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)
+        smtp.sendmail(EMAIL_SENDER, email_receiver, em.as_string())
+        # print(status_code)
+payload = {
+    "first name": "sikiru", 
+    "last name": "yusuff",
+    "email": "sikiru.yusuff@yahoo.com",
+    "phone": "07065264103", 
+    "sex": "male",
+    "country": "nigeria",
+    }
+add_user_request = requests.post(f'{API_ENDPOINT}/post', data=payload)
+if add_user_request.status_code == 200:
+    send_update('successfully posted', add_user_request.json)
+
+else:
+    send_update('failed to post request', add_user_request.json)
+
+req = requests.get(API_ENDPOINT)
+print(req._content)
+response = requests.post('https://httpbin.org/post', data=payload)
+print(response._content)
